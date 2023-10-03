b48c0dbb24b8c904d20e08dffe66fbd3d8a476bb
zhukovadariya <daaasha2112@gmail.com>
2023-10-03 23:30:47 +0300

    add connection

--- src/main/java/Client/Gamer.java
+++ src/main/java/Client/Gamer.java
@@ -7,7 +7,28 @@ package Client;
 // в цикле процедуры: считать направление(команду) с консоли
 // отправка команды, пока не достигнем завершения
 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Gamer {
+    public static void main(String[] args) {
+        String serverAddress = "127.0.0.1";
+        int serverPort = 12345;
 
+        try {
 
+            Scanner scanner = new Scanner(System.in);
+
+            Socket socket = new Socket(serverAddress, serverPort);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
